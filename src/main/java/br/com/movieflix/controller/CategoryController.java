297272b026cfa6da402cbd8daaf7d4fa2a17e0a7
Navarro Rangel <navarrorangel1@hotmail.com>
2025-08-05 16:45:38 -0300

    :sparkles: feat: adicionando rotas no controller ja refatoradas com mapper

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -1,8 +1,12 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.controller.request.CategoryRequest;
+import br.com.movieflix.controller.response.CategoryResponse;
 import br.com.movieflix.entity.Category;
+import br.com.movieflix.mapper.CategoryMapper;
 import br.com.movieflix.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,28 +25,32 @@ public class CategoryController {
     }
 
     @GetMapping()
-    public List<Category> getAllCategories(){
-        return categoryService.findAll();
+    public ResponseEntity<List<CategoryResponse>> getAllCategories(){
+        return ResponseEntity.ok(categoryService.findAll()
+                .stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList());
     }
 
     @PostMapping()
-    public Category saveCategory(@RequestBody Category category){
-        return categoryService.saveCategory(category);
+    public ResponseEntity< CategoryResponse> saveCategory(@RequestBody CategoryRequest request){
+        Category newCategory = CategoryMapper.toCategory(request);
+        Category savedCategory = categoryService.saveCategory(newCategory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CategoryMapper.toCategoryResponse(savedCategory));
     }
 
     @GetMapping("/{id}")
-    public Category getCategoryId(@PathVariable Long id){
-        Optional<Category> optionalCategory = categoryService.findByid(id);
-        if(optionalCategory.isPresent()){
-            return optionalCategory.get();
-        } else{
-            return null;
-        }
+    public ResponseEntity<CategoryResponse> getCategoryId(@PathVariable Long id) {
+        return categoryService.findByid(id)
+                .map(category -> ResponseEntity.ok(CategoryMapper.toCategoryResponse(category)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
+
     @DeleteMapping("/{id}")
-    public void deleteCategory(@PathVariable Long id){
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id){
         categoryService.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
---
+++ src/main/java/br/com/movieflix/controller/request/CategoryRequest.java
@@ -0,0 +1,4 @@
+package br.com.movieflix.controller.request;
+
+public class CategoryRequest {
+}
