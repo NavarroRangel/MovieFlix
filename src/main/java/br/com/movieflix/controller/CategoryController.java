366d922c2e791b70b3d76c34eb9ea7e365f51bb0
Navarro Rangel <navarrorangel1@hotmail.com>
2025-08-05 10:22:06 -0300

    :sparkles: feat: adicionando Findall Categories tanto nas rotas de controler quanto a regra de negocio em service

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -1,8 +1,30 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.entity.Category;
+import br.com.movieflix.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@RestController()
+@RequestMapping("/movieflix/category")
+
 public class CategoryController {
+
+    private final CategoryService categoryService;
+    private CategoryService retu;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping()
+    public List<Category> getAllCategories(){
+        return categoryService.findAll();
+    }
+
 
 }
--- src/main/java/br/com/movieflix/service/CategoryService.java
+++ src/main/java/br/com/movieflix/service/CategoryService.java
@@ -1,8 +1,20 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Category;
+import br.com.movieflix.repositoiry.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+    private final CategoryRepository repository;
 
+    public CategoryService(CategoryRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
 }
