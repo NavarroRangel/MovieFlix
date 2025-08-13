24d80d0aadeec05a521d07f0019667e7c1acd4c8
Navarro <navarrorangel1@hotmail.com>
2025-08-13 09:57:06 -0300

    :sparkles: feat: finalizando as rotas de MovieController, alterações na service para criar de serviços de buscaPorId, Na MovieRequest E MovieResponse foram feitas alterações para formatar a data no seguinte formato (dd/MM/yyyy)

--- src/main/java/br/com/movieflix/service/MovieService.java
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -1,24 +1,54 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Category;
 import br.com.movieflix.entity.Movie;
+import br.com.movieflix.entity.Streaming;
 import br.com.movieflix.repositoiry.MovieRepository;
+import org.hibernate.boot.model.source.internal.hbm.ModelBinder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
     private final MovieRepository movieRepository;
+    private final CategoryService  categoryService;
+    private final StreamingService streamingService;
 
-    public MovieService(MovieRepository movieRepository) {
+    public MovieService(MovieRepository movieRepository, CategoryService categoryService, StreamingService streamingService) {
         this.movieRepository = movieRepository;
+        this.categoryService = categoryService;
+        this.streamingService = streamingService;
     }
 
     public Movie save(Movie movie){
+        movie.setCateogries(this.findCategories(movie.getCateogries()));
+        movie.setStreamings(this.findStreaming(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(Long id){
+        return movieRepository.findById(id);
+    }
+
+    private List<Category> findCategories(List<Category> categories){
+        List<Category> categoryFound = new ArrayList<>();
+        for (Category category : categories) {
+            categoryService.findByid(category.getId()).ifPresent(categoryFound::add);
+        }
+        return categoryFound;
+    }
+    private List<Streaming> findStreaming(List<Streaming> streamings){
+        List<Streaming> streamingFound = new ArrayList<>();
+        for (Streaming streaming : streamings) {
+            streamingService.findByid(streaming.getId()).ifPresent(streamingFound::add);
+        }
+        return streamingFound;
     }
 }
