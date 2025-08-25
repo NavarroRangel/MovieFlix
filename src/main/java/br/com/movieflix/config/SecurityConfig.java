310f3e27eb20c2c5e6df95758512e4d436337908
Navarro <navarrorangel1@hotmail.com>
2025-08-25 09:24:51 -0300

    :sparkles: feat: adicionando springSecurity config e configurando acesso das rotas

---
+++ src/main/java/br/com/movieflix/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package br.com.movieflix.config;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sessiong -> sessiong.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,"/movieflix/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/movieflix/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                //.addFilter()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
