46980095eea38b3e695ed4a1d0636d7e2fd59911
Navarro <navarrorangel1@hotmail.com>
2025-08-26 09:28:49 -0300

    :sparkles: feat: adicionando bearer token para validação do login!

--- src/main/java/br/com/movieflix/controller/AuthController.java
+++ src/main/java/br/com/movieflix/controller/AuthController.java
@@ -1,8 +1,10 @@
 package br.com.movieflix.controller;
 
 
+import br.com.movieflix.config.TokenService;
 import br.com.movieflix.controller.request.LoginRequest;
 import br.com.movieflix.controller.request.UserRequest;
+import br.com.movieflix.controller.response.LoginResponse;
 import br.com.movieflix.controller.response.UserResponse;
 import br.com.movieflix.entity.User;
 import br.com.movieflix.mapper.UserMapper;
@@ -25,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final  UserService userService;
     private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody UserRequest userRequest){
@@ -33,10 +36,12 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request){
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
         UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(request.email(),request.password());
         Authentication authenticate = authenticationManager.authenticate(userAndPass);
         User user =(User) authenticate.getPrincipal();
+        String token = tokenService.generateToken(user);
+        return ResponseEntity.ok(new LoginResponse(token));
 
         //
     }
